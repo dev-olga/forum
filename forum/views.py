2c9c03122eb9f2ddbd781cabb197f1c6492ae5b6
root <root@debian>
2015-02-17 10:59:03 -0500

    Add file with migrate command and initial data

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'title': 'Forum'}
+    return render(request, 'forum/index.html', context)
 
 
